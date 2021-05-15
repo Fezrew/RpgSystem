f75453607fa0732eab7402626cb45a325b5f4459
Dylan White <dylanhwite@Gmail.com>
2021-05-15 14:43:21 +0930

    Started making RPG System

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Range(0, 150)]
+    [Tooltip("Default: 60")]
+    public int mouseSensitivity = 30;
+
+    public Transform player;
+
+    private float xRotation = 0f;
+
+    private float mouseX = 0f;
+    private float mouseY = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Cursor.lockState = CursorLockMode.None;
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            mouseX = Input.GetAxis("Mouse X") * (mouseSensitivity * 100) * Time.deltaTime;
+            mouseY = Input.GetAxis("Mouse Y") * (mouseSensitivity * 100) * Time.deltaTime;
+        }
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        player.Rotate(Vector3.up * mouseX);
+    }
+}
